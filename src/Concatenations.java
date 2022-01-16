396a0ae8d44ac7032fe813f8e891e095606a9d2a
MeAdarshMishra <adarsh.mishra_cs19@gla.ac.in>
2022-01-16 23:57:19 +0530

    Concatenations in java

---
+++ src/Concatenations.java
@@ -0,0 +1,8 @@
+public class Concatenations {
+    public static void main(String[] args) {
+        String FirstName="MeAadarsh";
+        String LastName="Mishra";
+        String fullname=FirstName+"&"+LastName;
+        System.out.println(fullname);
+    }
+}
