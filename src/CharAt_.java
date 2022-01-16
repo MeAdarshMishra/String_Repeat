444ea08a6e0a2165e6327b7e97e8c8273d80ae11
MeAdarshMishra <adarsh.mishra_cs19@gla.ac.in>
2022-01-17 00:39:00 +0530

    Concatenations in java

---
+++ src/CharAt_.java
@@ -0,0 +1,15 @@
+import java.sql.SQLOutput;
+
+public class CharAt_ {
+    public static void main(String[] args) {
+        String name="MeAdarshMishra";
+        System.out.println(name.length());
+        for (int i = 0; i < name.length(); i++) {
+            System.out.println(name.charAt(i));
+            
+        }
+
+
+
+    }
+}
