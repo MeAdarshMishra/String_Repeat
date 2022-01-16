77b8c9d768e6d5ad316e797bb43c8aeffa3cda5b
MeAdarshMishra <adarsh.mishra_cs19@gla.ac.in>
2022-01-17 00:44:19 +0530

    Compare in Strings

---
+++ src/Compare_.java
@@ -0,0 +1,12 @@
+public class Compare_ {
+    public static void main(String[] args) {
+        String a="Adarsh";
+        String b="Adarsh";
+        if (a.compareTo(b)==0){
+            System.out.println("Strings are equal");
+        }
+        else{
+            System.out.println("are not equal");
+        }
+    }
+}
