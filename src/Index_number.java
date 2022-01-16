6e3a3f939f997abea2e39231844ca1c750ea2e6d
MeAdarshMishra <adarsh.mishra_cs19@gla.ac.in>
2022-01-17 00:53:54 +0530

    Index

---
+++ src/Index_number.java
@@ -0,0 +1,10 @@
+public class Index_number {
+    public static void main(String[] args) {
+        String name="My name is AdarshMishra";
+        String index=name.substring(11,name.length());
+        System.out.println(index);
+        String StartToEnd=name.substring(11,17);
+        System.out.println(StartToEnd);
+
+    }
+}
