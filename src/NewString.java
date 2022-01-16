e7af326abad490064e528a985fa4d928e7fe5f81
MeAdarshMishra <adarsh.mishra_cs19@gla.ac.in>
2022-01-16 22:09:10 +0530

    Initial commit

---
+++ src/NewString.java
@@ -0,0 +1,10 @@
+public class NewString {
+    public static void main(String[] args) {
+        String a="Me";
+        String b="Adarsh";
+        String c="Mishra";
+        String d=a+b+c;
+        System.out.println("System.out.println("+d+")");
+    }
+
+}
